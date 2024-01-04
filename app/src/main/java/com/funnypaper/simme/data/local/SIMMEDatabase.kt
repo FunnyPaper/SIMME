3c67d4c25f6d939a63dee89c51d117b3bff40b29
CJ <78097248+FunnyPaper@users.noreply.github.com>
2024-01-04 13:31:13 +0100

    Added database for daos.

---
+++ app/src/main/java/com/funnypaper/simme/data/local/SIMMEDatabase.kt
@@ -0,0 +1,56 @@
+package com.funnypaper.simme.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.funnypaper.simme.data.local.converter.PointConverter
+import com.funnypaper.simme.data.local.converter.SplineConverter
+import com.funnypaper.simme.data.local.dao.AudioDao
+import com.funnypaper.simme.data.local.dao.BoardDao
+import com.funnypaper.simme.data.local.dao.MetaDataDao
+import com.funnypaper.simme.data.local.dao.NoteDao
+import com.funnypaper.simme.data.local.dao.ProjectDao
+import com.funnypaper.simme.data.local.dao.RankDao
+import com.funnypaper.simme.data.local.dao.SplineDao
+import com.funnypaper.simme.data.local.dao.TimingDao
+import com.funnypaper.simme.data.local.entity.AudioEntity
+import com.funnypaper.simme.data.local.entity.BoardEntity
+import com.funnypaper.simme.data.local.entity.MetaDataEntity
+import com.funnypaper.simme.data.local.entity.NoteEntity
+import com.funnypaper.simme.data.local.entity.ProjectEntity
+import com.funnypaper.simme.data.local.entity.RankEntity
+import com.funnypaper.simme.data.local.entity.SplineEntity
+import com.funnypaper.simme.data.local.entity.TimingEntity
+
+@Database(
+    entities = [
+        BoardEntity::class,
+        MetaDataEntity::class,
+        NoteEntity::class,
+        ProjectEntity::class,
+        RankEntity::class,
+        SplineEntity::class,
+        AudioEntity::class,
+        TimingEntity::class
+    ],
+    version = 1,
+    exportSchema = false
+)
+@TypeConverters(
+    PointConverter::class,
+    SplineConverter::class
+)
+abstract class SIMMEDatabase : RoomDatabase() {
+    abstract fun boardDao(): BoardDao
+    abstract fun noteDao(): NoteDao
+    abstract fun projectDao(): ProjectDao
+    abstract fun rankDao(): RankDao
+    abstract fun splineDao(): SplineDao
+    abstract fun metaDataDao(): MetaDataDao
+    abstract fun audioDao(): AudioDao
+    abstract fun timingDao(): TimingDao
+
+    companion object {
+        const val NAME = "simme_database"
+    }
+}
