f73cbbb2c1f00fe54e824d0b2bf052ad8569ee96
CJ <78097248+FunnyPaper@users.noreply.github.com>
2024-01-14 14:15:07 +0100

    Changed color palette (theme). Added new font family (jura).

--- app/src/main/java/com/funnypaper/simme/ui/theme/Type.kt
+++ app/src/main/java/com/funnypaper/simme/ui/theme/Type.kt
@@ -2,9 +2,44 @@ package com.funnypaper.simme.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.funnypaper.simme.R
+
+val jura_regular = Font(
+    resId = R.font.jura_regular,
+    weight = FontWeight.Normal
+)
+
+val jura_bold = Font(
+    resId = R.font.jura_bold,
+    weight = FontWeight.Bold
+)
+
+val jura_light = Font(
+    resId = R.font.jura_light,
+    weight = FontWeight.Light
+)
+
+val jura_medium = Font(
+    resId = R.font.jura_medium,
+    weight = FontWeight.Medium
+)
+
+val jura_semibold = Font(
+    resId = R.font.jura_semibold,
+    weight = FontWeight.SemiBold
+)
+
+val jura = FontFamily(
+    jura_light,
+    jura_regular,
+    jura_medium,
+    jura_semibold,
+    jura_bold
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -14,21 +49,12 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+    headlineLarge = TextStyle(
+        fontFamily = jura,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 32.sp,
+        lineHeight = 32.sp * 1.25,
+        letterSpacing = 0.sp,
     )
-    */
 )
